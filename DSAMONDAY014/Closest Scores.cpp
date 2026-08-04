02bd8392ac7ffdbbdd34f62c537a0b1db08f684e
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-04 09:26:48 +0530

    Create Closest Scores.cpp

---
+++ DSAMONDAY014/Closest Scores.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  int ans = INT_MAX;
+  for (int i = 1; i < n; i++) {
+    int diff = a[i] - a[i - 1];
+    ans = min(ans, diff);
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
