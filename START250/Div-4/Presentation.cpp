0700fca33001161425852f763734e7bd121ca6a6
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-06 14:30:21 +0530

    Create Presentation.cpp

---
+++ START250/Div-4/Presentation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << 20 - n << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
